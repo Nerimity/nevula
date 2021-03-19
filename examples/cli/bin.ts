4ed3a5c095fdbb89395824541b288311a9b0e941
brecert <11599528+Brecert@users.noreply.github.com>
2021-03-19 13:00:55 -0400

    add simple cli html output example

---
+++ examples/cli/bin.ts
@@ -0,0 +1,86 @@
+import h from "https://cdn.skypack.dev/vhtml?dts";
+import {
+  addTextSpans,
+  Entity,
+  parseMarkup,
+  Span,
+  UnreachableCaseError,
+} from "../../nevula.ts";
+
+interface Context {
+  text: string;
+}
+
+const sliceText = (ctx: Context, span: Span) =>
+  ctx.text.slice(span.start, span.end);
+
+const transformEntities = (entities: Entity[], ctx: Context) =>
+  entities.map((e) => transformEntity(e, ctx));
+
+function transformEntity(entity: Entity, ctx: Context): string {
+  switch (entity.type) {
+    case "text": {
+      if (entity.entities.length > 0) {
+        return transformEntities(entity.entities, ctx).join("");
+      } else {
+        return sliceText(ctx, entity.outerSpan);
+      }
+    }
+    case "bold":
+    case "italic":
+    case "underline":
+    case "strikethrough":
+      return h(entity.type[0], {}, transformEntities(entity.entities, ctx));
+    case "code":
+      return h("code", {}, transformEntities(entity.entities, ctx));
+    case "codeblock":
+      return h(
+        "pre",
+        {},
+        h(
+          "code",
+          {},
+          transformEntities(entity.entities, ctx),
+        ),
+      );
+    case "blockquote":
+      return h("blockquote", {}, transformEntities(entity.entities, ctx));
+    case "custom": {
+      return transformCustomEntity(entity, ctx);
+    }
+    default: {
+      throw new UnreachableCaseError(entity["type"] as never);
+    }
+  }
+}
+
+function transformCustomEntity(enttiy: Entity, ctx: Context) {
+  return sliceText(ctx, enttiy.outerSpan);
+}
+
+// actual cli app
+
+const [filePath, outPath] = Deno.args;
+
+if (!filePath) {
+  throw new Error(`filePath must be specified`);
+}
+
+const file = await Deno.readTextFile(filePath);
+const entity = addTextSpans(parseMarkup(file));
+const html = `
+<html>
+  <head>
+    <title>nevula markup cli example</title>
+  </head>
+  <body style="white-space: pre-line">
+    ${transformEntity(entity, { text: file })}
+  </body>
+</html>
+`;
+
+if (outPath) {
+  Deno.writeTextFileSync(outPath, html);
+} else {
+  console.log(html);
+}
