aa011959fe9f3d9dcdacef2ed5ae14a1e290db34
brecert <11599528+Brecert@users.noreply.github.com>
2021-04-30 09:38:42 -0400

    v0.8.2
    - forgot to build for v0.8.1
    - cleaned up publish folder

--- publish/node/markup.js
+++ publish/node/markup.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.addTextSpans = exports.parseMarkup = exports.partition = exports.UnreachableCaseError = exports.containsSpan = void 0;
 // generated from: https://github.com/brecert/unicode-emoji-regex
-const EMOJI_SEQUENCE = /(?:(?:(?:(?:\p{Emoji})(?:\u{FE0F}))|(?:(?:\p{Emoji_Modifier_Base})(?:\p{Emoji_Modifier}))|(?:\p{Emoji}))(?:[\u{E0020}-\u{E007E}]+)(?:\u{E007F}))|(?:(?:(?:(?:\p{Emoji_Modifier_Base})(?:\p{Emoji_Modifier}))|(?:(?:\p{Emoji})(?:\u{FE0F}))|(?:\p{Emoji}))(?:(?:\u{200d})(?:(?:(?:\p{Emoji_Modifier_Base})(?:\p{Emoji_Modifier}))|(?:(?:\p{Emoji})(?:\u{FE0F}))|(?:\p{Emoji})))+)|(?:(?:(?:\p{Regional_Indicator})(?:\p{Regional_Indicator}))|(?:(?:\p{Emoji_Modifier_Base})(?:\p{Emoji_Modifier}))|(?:[0-9#*]\u{FE0F}\u{20E3})|(?:(?:\p{Emoji})(?:\u{FE0F})))/u;
+const EMOJI = /(?:(?:(?:(?:(?:\p{Emoji})(?:\u{FE0F}))|(?:(?:\p{Emoji_Modifier_Base})(?:\p{Emoji_Modifier}))|(?:\p{Emoji}))(?:[\u{E0020}-\u{E007E}]+)(?:\u{E007F}))|(?:(?:(?:(?:\p{Emoji_Modifier_Base})(?:\p{Emoji_Modifier}))|(?:(?:\p{Emoji})(?:\u{FE0F}))|(?:\p{Emoji}))(?:(?:\u{200d})(?:(?:(?:\p{Emoji_Modifier_Base})(?:\p{Emoji_Modifier}))|(?:(?:\p{Emoji})(?:\u{FE0F}))|(?:\p{Emoji})))+)|(?:(?:(?:\p{Regional_Indicator})(?:\p{Regional_Indicator}))|(?:(?:\p{Emoji_Modifier_Base})(?:\p{Emoji_Modifier}))|(?:[0-9#*]\u{FE0F}\u{20E3})|(?:(?:\p{Emoji})(?:\u{FE0F}))))|\p{Emoji_Presentation}|\p{Extended_Pictographic}/u;
 /** Checks if `largeSpan` can contain `smallSpan` */
 function containsSpan(largeSpan, smallSpan) {
     return largeSpan.start <= smallSpan.start && smallSpan.end <= largeSpan.end;
@@ -54,7 +54,7 @@ const TOKEN_PARTS = {
     code: /``/,
     spoiler: /\|\|/,
     link: /https?:\/\/\S+\.[\p{Alphabetic}\d\/\\#?=+&%@;!._~-]+/,
-    emoji: new RegExp(String.raw `${EMOJI_SEQUENCE.source}|\p{Emoji_Presentation}|\p{Extended_Pictographic}`, "u"),
+    emoji: EMOJI,
     custom_start: /\[(?:.|\w+):/,
     custom_end: /\]/,
     emoji_name: /:\w+:/,
